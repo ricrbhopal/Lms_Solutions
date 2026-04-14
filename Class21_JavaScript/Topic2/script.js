717749cbf51d711e89f6ce6dfeabb6342dcc6182
rajkumarvns <rajkumar966601@gmail.com>
2026-04-14 15:48:39 +0530

    Class21, Topic2 solutions

---
+++ Class21_JavaScript/Topic2/script.js
@@ -0,0 +1,99 @@
+// 1
+let num1 = 45;
+console.log(num1);
+
+// 2
+let name1 = "Rahul";
+
+// 3
+let isTrue = true;
+
+// 4
+let decimalNum = 98.75;
+// 5
+let isFalse = false;
+
+// 6
+let language = "JavaScript";
+
+// 7
+let year = 2026;
+
+// 8
+let emptyValue = null;
+
+// 9
+let noValue;
+
+// 10
+let company = "OpenAI";
+
+// 11
+let age = 20;
+
+// 12
+const rollNumber = 101;
+// 13
+var schoolName = "ABC School";
+
+// 14
+let amount = 500;
+amount = 700;
+
+// 15
+let isLoggedIn = true;
+
+// 16
+let studentName = "Raj Gupta";
+
+// 17
+let totalMarks = 450;
+
+// 18
+let isPresent = true;
+
+// 19
+let price = 199.99;
+
+// 20
+let country = "India";
+
+// 21
+let student = {
+    name: "Raj",
+    age: 20
+};
+
+// 22
+let value = 100;
+console.log(typeof value);
+
+// 23
+let numberValue = 50;
+numberValue = "50";
+
+// 24
+let exampleUndefined;
+
+// 25
+let exampleNull = null;
+let exampleUndefined2;
+console.log(exampleNull);       
+console.log(exampleUndefined2);
+
+// 26
+let myNumber = 10;
+let myString = "Hello";
+let myBoolean = false;
+
+// 27
+let favoriteSubject = "Mathematics";
+
+// 28
+let mobileNumber = "9876543210";
+
+// 29
+let isHoliday = false;
+
+// 30
+let cityName = "Mumbai";
