d4019029b669c4430981eeb44e5876fc9658ebe5
rajkumarvns <rajkumar966601@gmail.com>
2026-05-01 16:45:21 +0530

    ....

--- Class22_JavaScript/Topic1/script.js
+++ Class22_JavaScript/Topic1/script.js
@@ -1,29 +1,49 @@
 // 1. Print numbers from 1 to 5
-for (let i = 1; i <= 5; i++) { console.log(i); }
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
 
 // 2. Print numbers from 1 to 10
-for (let i = 1; i <= 10; i++) { console.log(i); }
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
 
 // 3. Print numbers from 10 to 1 (reverse)
-for (let i = 10; i >= 1; i--) { console.log(i); }
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
 
 // 4. Print even numbers from 2 to 10
-for (let i = 2; i <= 10; i += 2) { console.log(i); }
+for (let i = 2; i <= 10; i += 2) {
+  console.log(i);
+}
 
 // 5. Print odd numbers from 1 to 9
-for (let i = 1; i <= 9; i += 2) { console.log(i); }
+for (let i = 1; i <= 9; i += 2) {
+  console.log(i);
+}
 
 // 6. Table of 15 (up to 10 steps)
-for (let i = 1; i <= 10; i++) { console.log("15 x " + i + " = " + (15 * i)); }
+for (let i = 1; i <= 10; i++) {
+  console.log("15 x " + i + " = " + 15 * i);
+}
 
 // 7. Print numbers from 1 to 20 (increment of 2)
-for (let i = 1; i <= 20; i += 2) { console.log(i); }
+for (let i = 1; i <= 20; i += 2) {
+  console.log(i);
+}
 
 // 8. Print numbers from 5 to 50 (increment of 5)
-for (let i = 5; i <= 50; i += 5) { console.log(i); }
+for (let i = 5; i <= 50; i += 5) {
+  console.log(i);
+}
 
 // 9. Square of numbers from 1 to 10
-for (let i = 1; i <= 10; i++) { console.log(i * i); }
+for (let i = 1; i <= 10; i++) {
+  console.log(i * i);
+}
 
 // 10. Numbers from 1 to 10 divisible by 3
-for (let i = 1; i <= 10; i++) { if (i % 3 === 0) console.log(i); }
+for (let i = 1; i <= 10; i++) {
+  if (i % 3 === 0) console.log(i);
+}
