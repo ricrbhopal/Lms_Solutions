93a51e79f4625857cb1869b1de5c74f1ec76cbe2
rajkumarvns <rajkumar966601@gmail.com>
2026-05-14 12:35:24 +0530

    Add HTML and JavaScript files for To Do List application functionality

---
+++ Assignment_JS/To_Do_List_App/script.js
@@ -0,0 +1,49 @@
+function addNewTask() {
+  const taskToAdd = document.getElementById("newTask").value;
+  if (taskToAdd === "") {
+    alert("Write Your Task Name and then add task!");
+    return;
+  }
+  const LI = document.createElement("li");
+  LI.classList.add("my-3");
+
+  const DIV = document.createElement("div");
+  DIV.classList.add("d-flex", "align-items-center");
+
+  const SPAN1 = document.createElement("span");
+  SPAN1.innerText = taskToAdd;
+
+  const BUTTON = document.createElement("button");
+
+  BUTTON.classList.add(
+    "btn",
+    "btn-danger",
+    "ms-4",
+    "d-flex",
+    "gap-3",
+    "align-items-center",
+    "justify-content-center",
+  );
+
+  BUTTON.onclick = () => LI.remove();
+
+  const I = document.createElement("i");
+  I.classList.add("bi", "bi-trash");
+
+  const SPAN2 = document.createElement("span");
+  SPAN2.innerText = "Delete";
+
+  BUTTON.appendChild(I);
+  BUTTON.appendChild(SPAN2);
+
+  DIV.appendChild(SPAN1);
+  DIV.appendChild(BUTTON);
+
+  LI.appendChild(DIV);
+
+  document.getElementById("taskDiv").classList.remove("d-none");
+
+  document.getElementById("taskList").appendChild(LI);
+
+  document.getElementById("newTask").value = "";
+}
