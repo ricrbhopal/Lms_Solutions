d65768782e43b4e320e2d93d1dd306ef2ccf8a94
rajkumarvns <rajkumar966601@gmail.com>
2026-05-16 12:58:46 +0530

    Enhance task management functionality by improving task validation and adding task removal from local storage

--- Assignment_JS/To_Do_List_App/script.js
+++ Assignment_JS/To_Do_List_App/script.js
@@ -1,9 +1,8 @@
 function addNewTask() {
   const taskToAdd = document.getElementById("newTask").value;
-  if (taskToAdd === "") {
-    alert("Write Your Task Name and then add task!");
-    return;
-  }
+
+  if (!taskToAdd || !taskToAdd.trim()) return;
+
   const LI = document.createElement("li");
   LI.classList.add("my-3");
 
@@ -14,7 +13,6 @@ function addNewTask() {
   SPAN1.innerText = taskToAdd;
 
   const BUTTON = document.createElement("button");
-
   BUTTON.classList.add(
     "btn",
     "btn-danger",
@@ -24,8 +22,10 @@ function addNewTask() {
     "align-items-center",
     "justify-content-center",
   );
-
-  BUTTON.onclick = () => LI.remove();
+  BUTTON.onclick = () => {
+    LI.remove();
+    removeTaskFromLocalStorage(taskToAdd);
+  };
 
   const I = document.createElement("i");
   I.classList.add("bi", "bi-trash");
@@ -45,5 +45,85 @@ function addNewTask() {
 
   document.getElementById("taskList").appendChild(LI);
 
+  saveToLocalStorage(taskToAdd);
+
   document.getElementById("newTask").value = "";
+}
+
+function saveToLocalStorage(taskToAdd) {
+  const TaskArray = JSON.parse(localStorage.getItem("toDoTask")) || [];
+
+  TaskArray.push(taskToAdd);
+
+  const newTaskArray = JSON.stringify(TaskArray);
+
+  localStorage.setItem("toDoTask", newTaskArray);
+}
+
+function getTaskFromLocalStorage() {
+  const TaskArray = JSON.parse(localStorage.getItem("toDoTask")) || [];
+
+  console.log(TaskArray);
+
+  TaskArray.forEach((element) => {
+    const LI = document.createElement("li");
+    LI.classList.add("my-3");
+
+    const DIV = document.createElement("div");
+    DIV.classList.add("d-flex", "align-items-center");
+
+    const SPAN1 = document.createElement("span");
+    SPAN1.innerText = element;
+
+    const BUTTON = document.createElement("button");
+    BUTTON.classList.add(
+      "btn",
+      "btn-danger",
+      "ms-4",
+      "d-flex",
+      "gap-3",
+      "align-items-center",
+      "justify-content-center",
+    );
+    BUTTON.onclick = () => {
+      LI.remove();
+      removeTaskFromLocalStorage(element);
+    };
+
+    const I = document.createElement("i");
+    I.classList.add("bi", "bi-trash");
+
+    const SPAN2 = document.createElement("span");
+    SPAN2.innerText = "Delete";
+
+    BUTTON.appendChild(I);
+    BUTTON.appendChild(SPAN2);
+
+    DIV.appendChild(SPAN1);
+    DIV.appendChild(BUTTON);
+
+    LI.appendChild(DIV);
+
+    document.getElementById("taskDiv").classList.remove("d-none");
+
+    document.getElementById("taskList").appendChild(LI);
+  });
+}
+
+getTaskFromLocalStorage();
+
+function removeTaskFromLocalStorage(taskItem) {
+  console.log(taskItem);
+
+  const TaskArray = JSON.parse(localStorage.getItem("toDoTask")) || [];
+
+  console.log(TaskArray);
+
+  const newTaskArray = TaskArray.filter((item) => item !== taskItem);
+
+  console.log(newTaskArray);
+
+  const newTaskString = JSON.stringify(newTaskArray);
+
+  localStorage.setItem("toDoTask", newTaskString);
 }
